208ba3dee3d74ff02d521161a53d7ede851dce82
marcoLapiello <marcolapiello@gmail.com>
2025-01-29 14:50:16 +0100

    Concepts of "Template Variable" and @ViewChild introduced in the NewTicketComponent

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { FormControlComponent } from '../../../shared/form-control/form-control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, FormControlComponent],
+  imports: [ButtonComponent, FormControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
 
+  onSubmit(titleInput: string, textInput: string) {
+    console.log(titleInput, textInput);
+
+    this.form?.nativeElement.reset();
+  }
 }
